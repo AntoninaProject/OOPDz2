85b98fdf82c5efe1df23a8a108563ecee9f56e64
Antonina Petrova <ant2466@yandex.ru>
2023-07-14 12:59:25 +0300

    FileHandlerForFamilyTree

---
+++ FileHandlerForFamilyTree.java
@@ -0,0 +1,30 @@
+package model.family_tree;
+
+package model.save;
+
+import model.familyTree.FamilyTree;
+import model.human.Human;
+import model.save.base.FileHandler;
+
+public class FileHandlerForFamilyTree implements WritableForFamilyTree{
+    private String filePath = "tree.out";
+    private FileHandler fileHandler;
+
+    public FileHandlerForFamilyTree() {
+        fileHandler = new FileHandler();
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    public boolean save(FamilyTree<Human> tree) {
+        return fileHandler.save(tree, filePath);
+    }
+
+    @Override
+    public FamilyTree<Human> read() {
+        return (FamilyTree<Human>)fileHandler.read(filePath);
+    }
+}
